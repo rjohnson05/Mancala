dc2604f1fcb4917a94ccaac861d1c908ae653759
Hank Rugg <hrugg@carroll.edu>
2023-03-20 10:43:58 -0600

    updated main function in Game.java

--- src/mancala/Game.java
+++ src/mancala/Game.java
@@ -1,13 +1,12 @@
 package mancala;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;  // Import the Scanner class
-import java.util.Iterator;
-
+import java.util.Scanner;
 
 public class Game {
 	private List<Pit> storeList = new ArrayList<>();
 	private int currentPlayer;
+	private boolean playerGetsAnotherMove;
 
 	
 	public Game() {
@@ -15,49 +14,74 @@ public class Game {
 		resetBoard();
 	}
 	
-	private boolean getsAnotherMove(int selectedPitIndex) {
+	public boolean getsAnotherMove(int selectedPitIndex) {
 		Pit selectedPit = storeList.get(selectedPitIndex);
+		int marbleCount = selectedPit.getMarbleCount();
+		
+		if (selectedPit.getMarbleCount() == 0) {
+			return false;
+		}
+
 		// checking if the marbles get to the end of the list and will loop around
-		int endPit = selectedPit.getMarbleCount() + selectedPitIndex;
-		if (endPit > 13) {
-			endPit -= 13;
+		int endPitIndex = selectedPitIndex + marbleCount;
+		if (endPitIndex > 13) {
+			endPitIndex -= 13;
 		}
 		
 		// checking which store to check
 		if (currentPlayer == 0) {
-			if (endPit == 6) {
+			if (endPitIndex == 6) {
 				return true;
 			}
 		} else {
-			if (endPit == 13) {
+			if (endPitIndex == 13) {
 				return true;
 			}
 		} return false;
 	}
 	
-	private void move(int selectedPitIndex) {
+	
+	
+	public boolean move(int selectedPitIndex) {
+		if (storeList.get(selectedPitIndex).getSide() != currentPlayer) {
+			return false;
+		}
+		
+		if (storeList.get(selectedPitIndex).getMarbleCount() == 0) {
+			return false;
+		}
+		
 		Pit selectedPit = storeList.get(selectedPitIndex);
 		int marbleCount = selectedPit.getMarbleCount();
 		// Marbles can only be moved if there is at least 1 marble in the selected pit
 		if (marbleCount > 0) {
 			int currentPitIndex = selectedPitIndex;
 			// Move each marble in the selected pit to another pit
-			for (int i = 0; i < selectedPit.getMarbleCount(); i++) {
+			for (Marble marble : selectedPit.getMarbleList()) {
+				// skip the other players store
+				if (currentPlayer == 0) {
+					if (currentPitIndex == 12) {
+						currentPitIndex++;
+					}
+				}
+				// skip the other players store
+				if (currentPlayer == 1) {
+					if (currentPitIndex == 5) {
+						currentPitIndex++;
+					}
+				}
 				int nextPitIndex = currentPitIndex + 1;
 				// Makes sure the pit index loops around if at the end of the list
 				if (nextPitIndex < storeList.size()) {
 					// Moves the marble to the next pit on the board
-					Marble removedMarble = selectedPit.removeMarble(storeList.get(selectedPitIndex).getMarbleList().get(selectedPitIndex+i));
 					Pit nextPit = storeList.get(nextPitIndex);
-					System.out.println("Next pit = " + nextPit.getMarbleCount());
-					nextPit.addMarble(removedMarble);
-					System.out.println("After add = " + nextPit.getMarbleCount());
+					nextPit.addMarble(marble);
 				} else {
 					// After reaching the end of the list of pits, the marble is placed in the first pit,
 					// creating a wrap-around effect
-					Marble removedMarble = selectedPit.removeMarble(storeList.get(selectedPitIndex).getMarbleList().get(selectedPitIndex+i));
-					Pit nextPit = storeList.get(0);
-					nextPit.addMarble(removedMarble);
+					nextPitIndex = 0;
+					Pit nextPit = storeList.get(nextPitIndex);
+					nextPit.addMarble(marble);
 				}
 				currentPitIndex = nextPitIndex;
 			}
@@ -65,30 +89,32 @@ public class Game {
 			selectedPit.getMarbleList().clear();
 			selectedPit.setMarbleCount(0);
 		}
+		return true;
 	}
-	
-
-
 		
 	
-	private boolean hasWinner() {
+	public boolean hasWinner() {
 		// check the first side of the board for a winner
+		boolean side1Empty = true;
 		for (int i = 0; i < 6; i++) {
 			if (storeList.get(i).getMarbleCount() != 0) {
-				return false;
+				side1Empty = false;
 			}
 		}
 		// check the second side of the board for a winner
+		boolean side2Empty = true;
 		for (int i = 7; i < 13; i++) {
 			if (storeList.get(i).getMarbleCount() != 0) {
-				return false;
+				side2Empty = false;
 			}
-		} return true;
+		}
+		
+		return (side1Empty || side2Empty);
 	}
 	
 
 	
-	private void switchPlayer() {
+	public void switchPlayer() {
 		// if the current player is 0, switch it
 		if (currentPlayer == 0) {
 			currentPlayer = 1;
@@ -101,29 +127,29 @@ public class Game {
 		
 	}
 	
-	private void endGame() {
-		System.out.println("player" + getWinner() + "won the game!");
+	public void endGame() {
+		System.out.println("player " + getWinner() + " won the game! The score was, Player 0: " + storeList.get(6).getMarbleCount() + " to Player 2:  " + storeList.get(13).getMarbleCount());
 		resetBoard();
 	}
 	
-	private void resetBoard() {
+	public void resetBoard() {
 		// reset the first 6 pits
 		for (int i = 0; i < 6; i++) {
-			Pit pit = new Pit();
+			Pit pit = new Pit(4,0);
 			storeList.add(pit);
 		}
 		// reset the first store at index 6
-		storeList.add(new Pit());
+		storeList.add(new Pit(0, -1));
 		// reset the second 6 pits
 		for (int i = 0; i < 6; i++) {
-			Pit pit = new Pit();
+			Pit pit = new Pit(4,1);
 			storeList.add(pit);
 		}
 		// reset the second store at index 13
-		storeList.add(new Pit());
+		storeList.add(new Pit(0, -1));
 	}
 	
-	private int getWinner() {
+	public int getWinner() {
 		// compare the stores of each player
 		if (hasWinner()) {
 			// if player 0 has a greater amount in their store, return 0
@@ -135,28 +161,107 @@ public class Game {
 		
 	}
 	
+
 	
+	public boolean checkCapture(int selectedPitIndex) {
+		// get the selected pit
+		Pit selectedPit = storeList.get(selectedPitIndex);
+		//get the marble count of the selected pit
+		int marbleCount = selectedPit.getMarbleCount();
+		
+		if (marbleCount == 0) {
+			return false;
+		}
+		
+		// check if the end pit needs to loop around the board
+		int endPitIndex = selectedPitIndex + marbleCount;
+		if (endPitIndex > 13) {
+			endPitIndex -= 13;
+		}
+		// if the end pit is in either of the stores, it can not be captured
+		if (endPitIndex == 13 || endPitIndex == 6) {
+			return false;
+		}
+		// check if the end pit is on your side
+		Pit endPit = storeList.get(endPitIndex);
+		if (endPit.getSide() != currentPlayer) {
+			return false;
+		}
+		// if the end pit marble count is 0 then we have a capture
+		if (endPit.getMarbleCount() == 0) {
+			int capturePitIndex = 12 - endPitIndex;
+			Pit capturePit = storeList.get(capturePitIndex);
+			if (capturePit.getMarbleCount() == 0) {
+				return false;
+			}
+			if (currentPlayer == 0) {
+				// then move the marbles to the correct store
+				int currentStore = storeList.get(6).getMarbleCount();
+				storeList.get(6).setMarbleCount(currentStore + 1 + capturePit.getMarbleCount());
+
+			} else {
+				int currentStore = storeList.get(6).getMarbleCount();
+				storeList.get(13).setMarbleCount(currentStore + 1 + capturePit.getMarbleCount());
+
+				
+			}
+			
+			// set both of the pits to a marble count of 0
+			capturePit.setMarbleCount(0);
+			storeList.get(endPitIndex - 1).setMarbleCount(0);
+
+			return true;
+			
+		} return false;
+	}
 	
+	public List<Pit> getStoreList() {
+		return storeList;
+	}
+	
+	public int getCurrentPlayer() {
+		return currentPlayer;
+	}
 	
 	
 	public static void main(String[] args) {
 		Game g = new Game();
-		int getSelectedIndex;
+		int getSelectedIndex = -1;
 		Scanner s = new Scanner(System.in);
 		while (!g.hasWinner()) {
+			System.out.println("Player " + g.currentPlayer + " is up.");
+			System.out.println((g.storeList.toString()));
 		    System.out.println("Enter selected pit you want to move");
 			getSelectedIndex = s.nextInt();
-			g.move(getSelectedIndex);
-			System.out.println("Moved");
-			System.out.println((g.storeList.toString()));
-			while(g.getsAnotherMove(getSelectedIndex)) {
+			g.playerGetsAnotherMove = g.getsAnotherMove(getSelectedIndex);
+			g.checkCapture(getSelectedIndex);
+			while(!g.move(getSelectedIndex)) {
+			    System.out.println("Select a valid pit!");
+				System.out.println((g.storeList.toString()));
+				getSelectedIndex = s.nextInt();
+				g.playerGetsAnotherMove = g.getsAnotherMove(getSelectedIndex);
+				g.checkCapture(getSelectedIndex);
+			}
+			while(g.playerGetsAnotherMove) {
+				System.out.println("Nice! Player " + g.currentPlayer + " gets another turn.");
+				System.out.println((g.storeList.toString()));
 			    System.out.println("Enter selected pit you want to move");
 				getSelectedIndex = s.nextInt();
-				g.move(getSelectedIndex);
+				g.playerGetsAnotherMove = g.getsAnotherMove(getSelectedIndex);
+				g.checkCapture(getSelectedIndex);
+				while(!g.move(getSelectedIndex)) {
+				    System.out.println("Select a valid pit!");
+					System.out.println((g.storeList.toString()));
+					getSelectedIndex = s.nextInt();
+					g.playerGetsAnotherMove = g.getsAnotherMove(getSelectedIndex);
+					g.checkCapture(getSelectedIndex);
+				}
 			}
 			g.switchPlayer();
+			System.out.println("____________________________");
 		}
 		g.endGame();
+		s.close();
 
 	}
 	
--- src/mancala/Pit.java
+++ src/mancala/Pit.java
@@ -1,32 +1,24 @@
 package mancala;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pit {
-	private int marbleCount = 4;
+	private int marbleCount;
 	private List<Marble> marblesContained = new ArrayList<>();
 	private int boardSide;
 	
-	public Pit(int marbleCount) {
-		this.marbleCount = marbleCount;
+	public Pit(int marbleCount, int boardSide) {
+		this.boardSide = boardSide;
 		for (int i = 0; i < marbleCount; i++) {
-			addMarble(new Marble());
-		}
-	}	
-	
-	public Pit() {
-		this.marbleCount = 4;
-		for (int i = 0; i < 4; i++) {
 			addMarble(new Marble());
 		}
 	}
 	
-	// added set board side
-	public void setBoardSide(int boardSide) {
-		this.boardSide = boardSide;
+	public Pit() {
+		for (int i = 0; i < 4; i++) {
+			addMarble(new Marble());
+		}
 	}
 	
 	public int getMarbleCount() {
@@ -41,16 +33,19 @@ public class Pit {
 		return marblesContained;
 	}
 	
+	public void setMarbleCount(int newMarbleCount) {
+		marbleCount = newMarbleCount;
+	}
+	
 	public void addMarble(Marble marble) {
 		marblesContained.add(marble);
+		marbleCount++;
 	}
 	
-	public Marble removeMarble(Marble marble) {
+	public void removeMarble(Marble marble) {
 		marblesContained.remove(marble);
-		return marble;
+		marbleCount--;
 	}
-	
-	
 	
 	public String toString() {
 		return Integer.toString(getMarbleCount());
@@ -63,38 +58,5 @@ public class Pit {
 		}
 		return arr;
 	}
-	
-	
-	public static void main(String[] args) {
-		System.out.println("Testing Pit class....");
-		Pit p = new Pit();
-		p.setBoardSide(1);
-		Marble m = new Marble();
-		// test 1
-		p.addMarble(m);
-		if (p.getMarbleCount() == 1) {
-			System.out.println("Marble Count true test passed...");
-		} else {
-			System.out.println("Marble Count true test failed...");
-		}
-		
-		Pit l = new Pit();
-		Marble n = new Marble();
-		// test 2
-		
-		if (p.getSide() == 1) {
-			System.out.println("Marble Count test passed...");
-		} else {
-			System.out.println("Marble Count test failed...");
-		}
-		
-
-	}
-
-	public void setMarbleCount(int i) {
-		this.marbleCount = i;
-	}
-
 
 }
-
