4c39cbac23f5190f8d4be4cd20092ad500235345
Hank Rugg <hrugg@carroll.edu>
2023-04-17 10:10:28 -0600

    fixed end game panel bug

--- src/mancala/Cardlayout.java
+++ src/mancala/Cardlayout.java
@@ -2,8 +2,11 @@ package mancala;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.*;
+
  
 /**
  * This is the controller for the program and allows the user to switch between 
@@ -11,8 +14,6 @@ import javax.swing.*;
  *
  */
 public class Cardlayout extends JFrame implements MouseListener {
-	
-
  
     // Declaration of objects of CardLayout class.
     public CardLayout card;
@@ -23,6 +24,7 @@ public class Cardlayout extends JFrame implements MouseListener {
     public SettingsPanel settings;
     public PlayPanel play;
     public EndGamePanel end;
+    private boolean gameOver;
  
     // Declaration of objects
     // of Container class.
@@ -75,6 +77,7 @@ public class Cardlayout extends JFrame implements MouseListener {
         c.add("play", play);
         
     }
+    
      
 
     /**
@@ -94,9 +97,62 @@ public class Cardlayout extends JFrame implements MouseListener {
      
             // Function to set default operation of JFrame.
             cl.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
     	
     }
+    
+    public void createEndGame() {
+            end = new EndGamePanel(play.getGame().getWinner(), play.getGame().getStoreList().get(6).getMarbleList().size(), play.getGame().getStoreList().get(13).getMarbleList().size() );
+            end.exitGameButton.addMouseListener(this);
+            end.playAgainButton.addMouseListener(this);
+            c.add("end", end);
+    		card.show(c, "end");
+    }
+    
+    
+	public void singlePlayerMove() {
+	Timer timer = new Timer();
+	TimerTask action = new TimerTask() {
+		public void run() {
+
+			int bestPitIndex = play.chooseOpponentPit();
+			play.playerMove(bestPitIndex);
+			if (play.getGame().hasWinner()) {
+				play.getGame().setWinner();
+    			try {
+					Thread.sleep(200);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				createEndGame();
+			}
+
+			// If the computer opponent doesn't land in their store, the timer is
+			// stopped and prevented from moving the player again
+			if (!play.getGame().getsAnotherMove()) {
+				timer.cancel();
+			}
+
+		}
+	};
+	if (play.getGame().hasWinner()) {
+		play.getGame().setWinner();
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		createEndGame();
+		
+	}
+	// The computer opponent's timer is started as soon as the human player has
+	// finished their turn
+	if (play.getGame().getCurrentPlayer() == 1 && !play.getGame().hasWinner()) {
+		timer.schedule(action, 1500, 1500);
+	}
+	
+}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
@@ -121,12 +177,18 @@ public class Cardlayout extends JFrame implements MouseListener {
     		card.show(c, "play");
     	}
     	else if (bclicked.getText() == " ") {
+    		if (play.singlePlayer) {
+    			singlePlayerMove();
+    		}
     		if (play.getGame().hasWinner()) {
-    	        end = new EndGamePanel(play.getGame().getWinner(), play.getGame().getStoreList().get(6).getMarbleList().size(), play.getGame().getStoreList().get(13).getMarbleList().size() );
-    	        end.exitGameButton.addMouseListener(this);
-    	        end.playAgainButton.addMouseListener(this);
-    	        c.add("end", end);
-    			card.show(c, "end");
+    			play.getGame().setWinner();
+    			try {
+					Thread.sleep(200);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+    			createEndGame();
     		}
     	}
     	else if (bclicked.getText() == "Play Again"){
@@ -135,7 +197,6 @@ public class Cardlayout extends JFrame implements MouseListener {
     		card.show(c, "settings");
     	}
     	
-		
 	}
 
 	@Override
--- src/mancala/PlayPanel.java
+++ src/mancala/PlayPanel.java
@@ -64,6 +64,10 @@ public class PlayPanel extends JPanel {
 	private JLabel marbleCountLabel = new JLabel();
 	private List<String> keysTyped = new ArrayList<String>();
 	public RoundButton[] pitButtons = new RoundButton[14];
+	
+
+	
+	public boolean turnIsOver = false;
 	public static boolean singlePlayer = false;
 
 	/**
@@ -220,7 +224,7 @@ public class PlayPanel extends JPanel {
 			// Set the background color
 			this.setBackground(new Color(228, 218, 199));
 		} catch (FileNotFoundException e) {
-			System.out.println("An specified image file cannot be found");
+			System.out.println("A specified image file cannot be found");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -344,9 +348,7 @@ public class PlayPanel extends JPanel {
 		 * the designated pit is moved successfully, the boolean true is returned. If
 		 * there are no marbles in the pit to move, false is returned
 		 */
-//		if (singlePlayer && game.getCurrentPlayer() == 1) {
-//			selectedPitIndex = 10;
-//		}
+
 		Pit selectedPit = game.getStoreList().get(selectedPitIndex);
 		int marbleCount = selectedPit.getMarbleList().size();
 		// Each marble within the selected pit is moved to the subsequent pits
@@ -608,6 +610,7 @@ public class PlayPanel extends JPanel {
 			if (game.checkCapture(endPitIndex)) {
 				moveCapturedMarbles(endPitIndex);
 				game.moveCapturedMarbles(endPitIndex);
+
 			}
 			if (!getsAnotherTurn) {
 				game.switchPlayer();
@@ -618,9 +621,44 @@ public class PlayPanel extends JPanel {
 		if (game.hasWinner()) {
 			game.setWinner();
 			repaint();
+			
 		}
 		repaint();
+		
 	}
+	
+	
+//	public void singlePlayerMove() {
+//		Timer timer = new Timer();
+//		TimerTask action = new TimerTask() {
+//			public void run() {
+//
+//				int bestPitIndex = chooseOpponentPit();
+//				playerMove(bestPitIndex);
+//				if (game.hasWinner()) {
+//					game.setWinner();
+//					
+//				}
+//
+//				// If the computer opponent doesn't land in their store, the timer is
+//				// stopped and prevented from moving the player again
+//				if (!game.getsAnotherMove()) {
+//					timer.cancel();
+//				}
+//
+//			}
+//		};
+//		// The computer opponent's timer is started as soon as the human player has
+//		// finished their turn
+//		if (game.getCurrentPlayer() == 1 && !game.hasWinner()) {
+//			timer.schedule(action, 1500, 1500);
+//		}
+//		
+//		if (game.hasWinner()) {
+//			game.setWinner();
+//		}
+//		
+//	}
 	
 	
 
@@ -642,7 +680,6 @@ public class PlayPanel extends JPanel {
 			 * of the moved marbles.
 			 */
 			public void mouseClicked(MouseEvent e) {
-
 					// On a mouse click, the marbles are moved and the player is changed
 					RoundButton buttonClicked = (RoundButton) e.getSource();
 					Pit currentPit = game.getStoreList().get(buttonClicked.getPitNumber());
@@ -652,40 +689,17 @@ public class PlayPanel extends JPanel {
 					// Only allows player to choose a pit on their side of the board
 					if (currentPit.getSide() == game.getCurrentPlayer()) {
 						playerMove(selectedPitIndex);
-
 					}
-
-
-
 					/*
 					 * Sets a timer for moving the computer opponent. After 2 seconds, a random pit
 					 * index from its side of the board is chosen. If the selected pit is empty, a
 					 * new random pit is selected until a non-empty pit is selected.
 					 */
-					if (singlePlayer) {
-					Timer timer = new Timer();
-					TimerTask action = new TimerTask() {
-						public void run() {
 
-							int bestPitIndex = chooseOpponentPit();
-							playerMove(bestPitIndex);
-
-							// If the computer opponent doesn't land in their store, the timer is
-							// stopped and prevented from moving the player again
-							if (!game.getsAnotherMove()) {
-								timer.cancel();
-							}
-						}
-					};
-					
-					// The computer opponent's timer is started as soon as the human player has
-					// finished their turn
-					if (game.getCurrentPlayer() == 1 && !game.hasWinner()) {
-						timer.schedule(action, 1500, 1500);
-					}
-					}
 					repaint();
+
 			}
+
 
 			/*
 			 * Specifies the actions to be taken when the mouse hovers over a pit button.
